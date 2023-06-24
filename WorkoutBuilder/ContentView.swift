2c5667e0a957c35c4986411a8928255b28b2662b
glzawalski <gabrielzawalski@gmail.com>
2023-06-24 03:23:53 -0300

    Create new project

---
+++ WorkoutBuilder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkoutBuilder
+//
+//  Created by Gabriel Zawalski on 20/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutBuilder/WorkoutBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutBuilderApp.swift
+//  WorkoutBuilder
+//
+//  Created by Gabriel Zawalski on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
