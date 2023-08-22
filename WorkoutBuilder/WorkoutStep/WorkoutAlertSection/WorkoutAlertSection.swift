f65f32547ea9aa9a4be175eaf684a00436b6d638
glzawalski <gabrielzawalski@gmail.com>
2023-08-22 11:43:54 -0300

    Creates reusable workout goal and alert sections && replace cooldown and warmup steps with generic workout step

---
+++ WorkoutBuilder/WorkoutStep/WorkoutAlertSection/WorkoutAlertSection.swift
@@ -0,0 +1,196 @@
+//
+//  WorkoutAlertSection.swift
+//  WorkoutBuilder
+//
+//  Created by Gabriel Zawalski on 21/08/23.
+//
+
+import SwiftUI
+import WorkoutKit
+
+struct WorkoutAlertSection: View {
+    @Binding var type: AlertType?
+    @Binding var unit: AlertUnit?
+    @Binding var metric: AlertMetric?
+
+    @Binding var thresholdValue: Double
+    @Binding var rangeMinValue: Double
+    @Binding var rangeMaxValue: Double
+    @Binding var zoneValue: Int
+
+    var body: some View {
+        Section("Alert") {
+            typePicker
+        }
+    }
+}
+
+extension WorkoutAlertSection {
+    var typePicker: some View {
+        Picker("Type", selection: $type) {
+            ForEach(AlertType.allCases, id: \.self) { item in
+                Text("\(item.rawValue)")
+                    .tag(item)
+            }
+
+            switch type {
+            case .cadenceRange:
+                frequencyUnitPicker
+                rangeValueInput
+
+            case .cadenceThreshold:
+                frequencyUnitPicker
+                thresholdValueInput
+
+            case .heartRateRange:
+                frequencyUnitPicker
+                rangeValueInput
+
+            case .heartRateZone:
+                frequencyUnitPicker
+                zoneValueInput
+
+            case .powerRange:
+                powerUnitPicker
+                rangeValueInput
+
+            case .powerThreshold:
+                powerUnitPicker
+                thresholdValueInput
+
+            case .powerZone:
+                powerUnitPicker
+                zoneValueInput
+
+            case .speedRange:
+                speedUnitPicker
+                rangeValueInput
+                metricPicker
+
+            case .speedThreshold:
+                speedUnitPicker
+                thresholdValueInput
+                metricPicker
+
+            case .none:
+                EmptyView()
+            }
+        }
+    }
+
+    var frequencyUnitPicker: some View {
+        Picker("Unit", selection: $unit) {
+            ForEach(AlertUnit.frequencyCases, id: \.self) { item in
+                Text("\(item.rawValue)")
+                    .tag(item)
+            }
+        }
+    }
+
+    var powerUnitPicker: some View {
+        Picker("Unit", selection: $unit) {
+            ForEach(AlertUnit.powerCases, id: \.self) { item in
+                Text("\(item.rawValue)")
+                    .tag(item)
+            }
+        }
+    }
+
+    var speedUnitPicker: some View {
+        Picker("Unit", selection: $unit) {
+            ForEach(AlertUnit.speedCases, id: \.self) { item in
+                Text("\(item.rawValue)")
+                    .tag(item)
+            }
+        }
+    }
+
+    var metricPicker: some View {
+        Picker("Metric", selection: $metric) {
+            ForEach(AlertMetric.allCases, id: \.self) { item in
+                Text("\(item.rawValue)")
+                    .tag(item)
+            }
+        }
+    }
+
+    var thresholdValueInput: some View {
+        HStack {
+            Text("Target Value")
+            TextField("Target Value", value: $thresholdValue, formatter: NumberFormatter())
+                .multilineTextAlignment(.trailing)
+        }
+    }
+
+    var rangeValueInput: some View {
+        Group {
+            HStack {
+                Text("Min Value")
+                TextField("Range Min Value", value: $rangeMinValue, formatter: NumberFormatter())
+                    .multilineTextAlignment(.trailing)
+            }
+
+            HStack {
+                Text("Max Value")
+                TextField("Range Max Value", value: $rangeMaxValue, formatter: NumberFormatter())
+                    .multilineTextAlignment(.trailing)
+            }
+        }
+    }
+
+    var zoneValueInput: some View {
+        HStack {
+            Text("Zone Value")
+            TextField("Zone Value", value: $zoneValue, formatter: NumberFormatter())
+                .multilineTextAlignment(.trailing)
+        }
+    }
+}
+
+#Preview {
+    WorkoutAlertSection(type: .constant(.cadenceThreshold), unit: .constant(.hertz), metric: .constant(nil), thresholdValue: .constant(.zero), rangeMinValue: .constant(.zero), rangeMaxValue: .constant(.zero), zoneValue: .constant(.zero))
+}
+
+enum AlertType: String, CaseIterable {
+    case cadenceRange = "Cadence Range"
+    case cadenceThreshold = "Cadence Threshold"
+    case heartRateRange = "Heart Rate Range"
+    case heartRateZone = "Heart Rate Zone"
+    case powerRange = "Power Range"
+    case powerThreshold = "Power Threshold"
+    case powerZone = "Power Zone"
+    case speedRange = "Speed Range"
+    case speedThreshold = "Speed Threshold"
+}
+
+enum AlertUnit: String {
+    case megahertz = "Megahertz"
+    case kilohertz = "Kilohertz"
+    case hertz = "Hertz"
+
+    static var frequencyCases: [AlertUnit] {
+        [.megahertz, .kilohertz, .hertz]
+    }
+
+    case kilowatts = "Kilowatts"
+    case watts = "Watts"
+    case milliwatts = "Milliwatts"
+
+    static var powerCases: [AlertUnit] {
+        [.kilowatts, .watts, .milliwatts]
+    }
+
+    case metersPerSecond = "Meters per second"
+    case kilometersPerHour = "Kilometers per hour"
+    case milesPerHour = "Miles per hour"
+    case knots = "Knots"
+
+    static var speedCases: [AlertUnit] {
+        [.metersPerSecond, .kilometersPerHour, .milesPerHour, .knots]
+    }
+}
+
+enum AlertMetric: String, CaseIterable {
+    case average = "Average"
+    case current = "Current"
+}
---
+++ WorkoutBuilder/WorkoutStep/WorkoutGoalSection/WorkoutGoalSection.swift
@@ -0,0 +1,125 @@
+//
+//  WorkoutGoalSection.swift
+//  WorkoutBuilder
+//
+//  Created by Gabriel Zawalski on 21/08/23.
+//
+
+import SwiftUI
+
+struct WorkoutGoalSection: View {
+    @Binding var type: GoalType
+    @Binding var unit: GoalUnit?
+    @Binding var value: Double
+
+    var body: some View {
+        Section("Goal") {
+            typePicker
+
+            switch type {
+            case .distance:
+                lengthUnitPicker
+                valueInput
+
+            case .energy:
+                energyUnitPicker
+                valueInput
+
+            case .time:
+                timeUnitPicker
+                valueInput
+
+            case .open:
+                EmptyView()
+            }
+        }
+    }
+}
+
+extension WorkoutGoalSection {
+    var typePicker: some View {
+        Picker("Type", selection: $type) {
+            ForEach(GoalType.allCases, id: \.self) { item in
+                Text("\(item.rawValue)")
+                    .tag(item)
+            }
+        }
+    }
+
+    var lengthUnitPicker: some View {
+        Picker("Unit", selection: $unit) {
+            ForEach(GoalUnit.lengthCases, id: \.self) { item in
+                Text("\(item.rawValue)")
+                    .tag(item)
+            }
+        }
+    }
+
+    var energyUnitPicker: some View {
+        Picker("Unit", selection: $unit) {
+            ForEach(GoalUnit.energyCases, id: \.self) { item in
+                Text("\(item.rawValue)")
+                    .tag(item)
+            }
+        }
+    }
+
+    var timeUnitPicker: some View {
+        Picker("Unit", selection: $unit) {
+            ForEach(GoalUnit.timeCases, id: \.self) { item in
+                Text("\(item.rawValue)")
+                    .tag(item)
+            }
+        }
+    }
+
+    var valueInput: some View {
+        HStack {
+            Text("Value")
+            TextField("Goal Value", value: $value, formatter: NumberFormatter())
+                .multilineTextAlignment(.trailing)
+        }
+    }
+}
+
+#Preview {
+    WorkoutGoalSection(type: .constant(.open), unit: .constant(nil), value: .constant(.zero))
+}
+
+enum GoalType: String, CaseIterable {
+    case distance = "Distance"
+    case energy = "Energy"
+    case time = "Time"
+    case open = "Open"
+}
+
+enum GoalUnit: String {
+    case kilometers = "Kilometers"
+    case meters = "Meters"
+    case centimeters = "Centimeters"
+    case feet = "Feet"
+    case yards = "Yards"
+    case miles = "Miles"
+
+    static var lengthCases: [GoalUnit] {
+        [.kilometers, .meters, .centimeters, .feet, .yards, .miles]
+    }
+
+    case kilojoules = "Kilojoules"
+    case joules = "Joules"
+    case kilocalories = "Kilocalories"
+    case calories = "Calories"
+    case kilowattHours = "Kilowatt Hours"
+
+    static var energyCases: [GoalUnit] {
+        [.kilojoules, .joules, .kilocalories, .calories, .kilowattHours]
+    }
+
+    case hours = "Hours"
+    case minutes = "Minutes"
+    case seconds = "Seconds"
+
+    static var timeCases: [GoalUnit] {
+        [.hours, .minutes, .seconds]
+    }
+}
