8cace9fb8087a6444bb6da8fb54a263ad7889665
glzawalski <gabrielzawalski@gmail.com>
2024-02-01 23:03:51 -0300

    Fix WorkoutGoal description extension

--- WorkoutBuilder/Extensions/WorkoutGoalExtension.swift
+++ WorkoutBuilder/Extensions/WorkoutGoalExtension.swift
@@ -11,9 +11,10 @@ import WorkoutKit
 extension WorkoutGoal {
     var description: String {
         switch self {
-        case let .distance(quantity): return "Distance: \(quantity)"
-        case let .time(quantity): return "Time: \(quantity)"
-        case let .energy(quantity): return "Energy \(quantity)"
+        case let .distance(quantity, unit): return "Distance: \(quantity) \(unit)"
+        case let .time(quantity, unit): return "Time: \(quantity) \(unit)"
+        case let .energy(quantity, unit): return "Energy \(quantity) \(unit)"
+        case .open: return "No goal"
         @unknown default: return "No goal"
         }
     }
